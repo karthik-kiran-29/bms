031496c6971e039a213f2d0d87190a3c4ee52e0c
karthik-kiran-29 <karthikkiran029@gmail.com>
2025-11-17 18:28:38 +0530

    feat(page): mock user data for Home component and remove database client

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,8 @@
 import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
-import  client  from "@repo/db/client";
-
-const prisma = new client.PrismaClient();
 
 export default async function Home() {
-  const data = await prisma.user.findFirst();
+  const data = {id:1,username:"karthik"}
   return (
     <div >
       {data?.id}
